6a6eaaf15648d8ad7a2473bc36c88d56f17e25f0
Tozarf <fazatojuni@gmail.com>
2022-01-14 19:01:58 -0600

    manual tests added

---
+++ tests/Rectangle.js
@@ -0,0 +1,8 @@
+const { Rectangle } = require("../OOP/Rectangle");
+
+const rectangle = new Rectangle("yellow", true, 3.3, 14.2);
+// console.log(rectangle.getArea());
+console.log(rectangle.getColor());
+console.log(rectangle.isFilled());
+console.log(rectangle.getLength());
+console.log(rectangle.getWidth());
---
+++ tests/Shape.js
@@ -0,0 +1,5 @@
+const { Shape } = require("../OOP/Shape");
+
+const shape = new Shape("yellow", true);
+// console.log(shape.#color);
+console.log(shape.#filled);
